a2b55b143e1cc703f497fd46fd33e8f2d1fbae5f
Dmitry Egorov <adm2444@gmail.com>
2016-05-11 02:40:32 +0300

    All instructions implemented

--- code/main.c
+++ code/main.c
@@ -19,9 +19,13 @@ INSTRUCTIONS:
   (IMPLEMENTED) STR - stores register value into memory
   (IMPLEMENTED) MOV - moves value from register to register
   (IMPLEMENTED) ADD - adds value of register to register
+  (IMPLEMENTED) ADDS - adds value of register to register (N, Z, C, V)
   (IMPLEMENTED) SUB - subs value of register from register
+  (IMPLEMENTED) SUBS - subs value of register from register (N, Z, C, V)
   (IMPLEMENTED) MUL - multiplies register value by register value
+  (IMPLEMENTED) MULS - multiplies register value by register value (N, Z, C, V)
   (IMPLEMENTED) DIV - divides register value by register value
+  (IMPLEMENTED) DIVS - divides register value by register value (N, Z, C, V)
   (IMPLEMENTED) PUSH - pushes value of register on top of a stack
   (IMPLEMENTED) POP - pops value from top of a stack into register
   (IMPLEMENTED) SVC - interrupt call to operating system (HACKED)
@@ -29,24 +33,26 @@ INSTRUCTIONS:
   (IMPLEMENTED) EORS - exclusive or between register values (N, Z)
   (IMPLEMENTED) ORRS - logical or between register values (N, Z)
   (IMPLEMENTED) BICS - logical and between register value and !(register value) (N, Z)
-  TST - updates flags based on result of AND operation (N, Z)
-  LSLS - logical shift left << and update flags (N, Z, C)
-  LSRS - logical shift right >> and update flags (N, Z, C)
-  CMP - updates flags based on result of SUB operation (N, Z, C, V)
-  CMN - updates flags based on result of ADD operation (N, Z, C, V)
+  (IMPLEMENTED) TST - updates flags based on result of AND operation (N, Z)
+  (IMPLEMENTED) LSLS - logical shift left << and update flags (N, Z, C)
+  (IMPLEMENTED) LSRS - logical shift right >> and update flags (N, Z, C)
+  (IMPLEMENTED) CMP - updates flags based on result of SUB operation (N, Z, C, V)
+  (IMPLEMENTED) CMN - updates flags based on result of ADD operation (N, Z, C, V)
   (IMPLEMENTED) B - unconditional jump
   (IMPLEMENTED) BEQ - jump if equal
   (IMPLEMENTED) BNE - jump if not equal
-  BCS - jump if carry is set
-  BCC - jump if carry is not set
+  (IMPLEMENTED) BCS - jump if carry is set
+  (IMPLEMENTED) BCC - jump if carry is not set
   (IMPLEMENTED) BMI - jump if negative
   (IMPLEMENTED) BPL - jump if positive or zero
-  BVS - jump if overflow
-  BVC - jump if no overflow
-  BGE - jump if greater than or equal
-  BLT - jump if less than
-  BGT - jump if greater than
-  BLE - jump if less than or equal
+  (IMPLEMENTED) BVS - jump if overflow
+  (IMPLEMENTED) BVC - jump if no overflow
+  (IMPLEMENTED) BGE - jump if greater than or equal
+  (IMPLEMENTED) BLT - jump if less than
+  (IMPLEMENTED) BGT - jump if greater than
+  (IMPLEMENTED) BLE - jump if less than or equal
+  (IMPLEMENTED) NEGS - changes sign of a register value (N, Z, C, V)
+  (IMPLEMENTED) MVNS - bitwise not of a register value (N, Z)
   (IMPLEMENTED) NOP - does nothing
 */
 
@@ -84,6 +90,21 @@ INSTRUCTIONS:
 #define IBNE   0x16
 #define IBMI   0x17
 #define IBPL   0x18
+#define IBGE   0x19
+#define IBLT   0x1A
+#define IBGT   0x1B
+#define IBLE   0x1C
+#define ITST   0x1D
+#define ICMP   0x1E
+#define ICMN   0x1F
+#define IBCS   0x20
+#define IBCC   0x21
+#define IBVS   0x22
+#define IBVC   0x23
+#define ILSLS  0x24
+#define ILSRS  0x25
+#define INEGS  0x26
+#define IMVNS  0x27
 
 #define PROC_EXIT  0x10
 #define PROC_PRINT 0x11
@@ -96,6 +117,10 @@ struct _vm{
   int32 *code;
   int32 *stack;
 } vm;
+
+inline void updflags_nz();
+inline void updflags_nzc();
+inline void updflags_nzcv();
 
 int main()
 {
@@ -115,12 +140,15 @@ int main()
     ILDRM, 0x0, 0x41,
     ISVC, PROC_PRINT,*/
     
-    ILDR, 0x0, 0x31,
-    ILDR, 0x2, 0xFF000000,
-    ILDR, 0x1, 0x40000000,
-    INOP,
-    IBICS, 0x2, 0x1,
-    IBPL, 0x11,
+    /*ILDR, 0x0, 0x31,
+    ILDR, 0x2, 0x02,
+    //ILDR, 0x1, 0x01,
+    INOP, INOP, INOP,
+    ILSRS, 0x2, 0x1,
+    IBCS, 0x13,*/
+    
+    ILDR, 0x0, 0xFFFFFFCF,
+    INEGS, 0x0,
     ISVC, PROC_PRINT,
     ISVC, PROC_EXIT
   };
@@ -143,20 +171,18 @@ int main()
         vm.r[reg] = val;
       } break;
       
-      //STR - stores register value into memory
-      case ISTR:{
-        int32 mdest = vm.code[ vm.r[0xF]++ ];
-        int32 rsrc  = vm.code[ vm.r[0xF]++ ];
-        
-        vm.mem[mdest] = vm.r[rsrc];
-      } break;
-      
-      //LDRM - loads value from memory into register
       case ILDRM:{
         int32 rdest = vm.code[ vm.r[0xF]++ ];
         int32 msrc  = vm.code[ vm.r[0xF]++ ];
         
         vm.r[rdest] = vm.mem[msrc];
+      } break;
+      
+      case ISTR:{
+        int32 mdest = vm.code[ vm.r[0xF]++ ];
+        int32 rsrc  = vm.code[ vm.r[0xF]++ ];
+        
+        vm.mem[mdest] = vm.r[rsrc];
       } break;
       
       case IMOV:{
@@ -180,7 +206,20 @@ int main()
       MATHOP(IDIV, /)
       #undef MATHOP
       
-      //TODO(adm244): MATHOPs with flags changes
+      #define MATHOPS(OPCODE, OP)\
+      case OPCODE:{\
+        int32 rdest = vm.code[ vm.r[0xF]++ ];\
+        int32 rsrc  = vm.code[ vm.r[0xF]++ ];\
+        \
+        vm.r[rdest] OP= vm.r[rsrc];\
+        updflags_nzcv();\
+      } break;
+      
+      MATHOPS(IADDS, +)
+      MATHOPS(ISUBS, -)
+      MATHOPS(IMULS, *)
+      MATHOPS(IDIVS, /)
+      #undef MATHOPS
       
       case IPUSH:{
         int32 reg = vm.code[ vm.r[0xF]++ ];
@@ -192,9 +231,8 @@ int main()
       case IPOP:{
         int32 reg = vm.code[ vm.r[0xF]++ ];
         int32 sp = vm.r[0xE]--;
-        int32 val = vm.stack[sp];
         
-        vm.r[reg] = val;
+        vm.r[reg] = vm.stack[sp];
       } break;
       
       #define LOGOP(OPCODE, OP)\
@@ -203,18 +241,7 @@ int main()
         int32 rsrc  = vm.code[ vm.r[0xF]++ ];\
         \
         vm.r[rdest] = vm.r[rdest] OP vm.r[rsrc];\
-        \
-        if( vm.r[rdest] & 0x80000000 ){\
-          vm.flags = vm.flags | 0x80000000;\
-        } else{\
-          vm.flags = vm.flags & 0x7FFFFFFF;\
-        }\
-        \
-        if( vm.r[rdest] == 0x0 ){\
-          vm.flags = vm.flags | 0x40000000;\
-        } else{\
-          vm.flags = vm.flags & 0xBFFFFFFF;\
-        }\
+        updflags_nz();\
       } break;
       
       LOGOP(IANDS, &)
@@ -223,73 +250,197 @@ int main()
       LOGOP(IBICS, & ~)
       #undef LOGOP
       
-      case IB:{
-        int32 pc = vm.code[ vm.r[0xF] ];
-        vm.r[0xF] = pc;
+      case ITST:{
+        int32 op1 = vm.r[ vm.code[ vm.r[0xF]++ ] ];
+        int32 op2 = vm.r[ vm.code[ vm.r[0xF]++ ] ];
+        
+        _asm{
+          mov  eax, op1
+          test eax, op2
+        }
+        updflags_nz();
       } break;
       
-      //BEQ - jump if equal
+      case ICMP:{
+        int32 op1 = vm.r[ vm.code[ vm.r[0xF]++ ] ];
+        int32 op2 = vm.r[ vm.code[ vm.r[0xF]++ ] ];
+        
+        _asm{
+          mov eax, op1
+          cmp eax, op2
+        }
+        updflags_nzcv();
+      } break;
+      
+      case ICMN:{
+        int32 op1 = vm.r[ vm.code[ vm.r[0xF]++ ] ];
+        int32 op2 = vm.r[ vm.code[ vm.r[0xF]++ ] ];
+        
+        _asm{
+          mov eax, op1
+          add eax, op2
+        }
+        updflags_nzcv();
+      } break;
+      
+      case ILSLS:{
+        int32 rdest = vm.code[ vm.r[0xF]++ ];
+        int32 shift = vm.code[ vm.r[0xF]++ ];
+        int32 oprnd = vm.r[rdest];
+        
+        _asm{
+          mov eax, oprnd
+          mov ecx, shift
+          sal eax, cl
+          
+          lea ecx, vm.r
+          mov ebx, rdest
+          mov [ecx+4*ebx], eax
+        }
+        updflags_nzc();
+      } break;
+      
+      case ILSRS:{
+        int32 rdest = vm.code[ vm.r[0xF]++ ];
+        int32 shift = vm.code[ vm.r[0xF]++ ];
+        int32 oprnd = vm.r[rdest];
+        
+        _asm{
+          mov eax, oprnd
+          mov ecx, shift
+          sar eax, cl
+          
+          lea ecx, vm.r
+          mov ebx, rdest
+          mov [ecx+4*ebx], eax
+        }
+        updflags_nzc();
+      } break;
+      
+      case INEGS:{
+        int32 rdest = vm.code[ vm.r[0xF]++ ];
+        
+        vm.r[rdest] *= -1;
+        updflags_nzcv();
+      } break;
+      
+      case IMVNS:{
+        int32 rdest = vm.code[ vm.r[0xF]++ ];
+        
+        vm.r[rdest] = ~vm.r[rdest];
+        updflags_nz();
+      } break;
+      
+      case IB:{
+        vm.r[0xF] = vm.code[ ++vm.r[0xF] ];
+      } break;
+      
       case IBEQ:{
         if( vm.flags & 0x40000000 ){
-          int32 pc = vm.code[ vm.r[0xF]++ ];
-          vm.r[0xF] = pc;
+          vm.r[0xF] = vm.code[ ++vm.r[0xF] ];
         } else{
           vm.r[0xF]++;
         }
       } break;
       
-      //BNE - jump if not equal
       case IBNE:{
         if( vm.flags & 0x40000000 ){
           vm.r[0xF]++;
         } else{
-          int32 pc = vm.code[ vm.r[0xF]++ ];
-          vm.r[0xF] = pc;
+          vm.r[0xF] = vm.code[ ++vm.r[0xF] ];
         }
       } break;
       
-      //BMI - jump if negative
       case IBMI:{
         if( vm.flags & 0x80000000 ){
-          int32 pc = vm.code[ vm.r[0xF]++ ];
-          vm.r[0xF] = pc;
+          vm.r[0xF] = vm.code[ ++vm.r[0xF] ];
         } else{
           vm.r[0xF]++;
         }
       } break;
       
-      //BPL - jump if positive or zero
       case IBPL:{
         if( vm.flags & 0x80000000 ){
           vm.r[0xF]++;
         } else{
-          int32 pc = vm.code[ vm.r[0xF]++ ];
-          vm.r[0xF] = pc;
+          vm.r[0xF] = vm.code[ ++vm.r[0xF] ];
         }
       } break;
       
-      //TODO(adm244): implement those, look at N and Z flags
-      /*
-      //BGE - jump if greater than or equal
+      //TODO(adm244): V flag should be taken into account as well
+      //NOTE(adm244): page 393 of Intel architecture manual
+      
+      //N xor V = 0
       case IBGE:{
-        
+        if( (vm.flags & 0x40000000) ||
+           !(vm.flags & 0x80000000) ){
+          vm.r[0xF] = vm.code[ ++vm.r[0xF] ];
+        } else{
+          vm.r[0xF]++;
+        }
       } break;
       
-      //BLT - jump if less than
+      //N xor V = 1
       case IBLT:{
-        
+        if( !(vm.flags & 0x40000000) &&
+             (vm.flags & 0x80000000) ){
+          vm.r[0xF] = vm.code[ ++vm.r[0xF] ];
+        } else{
+          vm.r[0xF]++;
+        }
       } break;
       
-      //BGT - jump if greater than
+      //(N xor V) or Z = 0
       case IBGT:{
-        
+        if( !(vm.flags & 0x40000000) &&
+            !(vm.flags & 0x80000000) ){
+          vm.r[0xF] = vm.code[ ++vm.r[0xF] ];
+        } else{
+          vm.r[0xF]++;
+        }
       } break;
       
-      //BLE - jump if less than or equal
+      //(N xor V) or Z = 1
       case IBLE:{
-        
+        if( (vm.flags & 0x40000000) ||
+            (vm.flags & 0x80000000) ){
+          vm.r[0xF] = vm.code[ ++vm.r[0xF] ];
+        } else{
+          vm.r[0xF]++;
+        }
       } break;
-      */
+      
+      case IBCS:{
+        if( vm.flags & 0x20000000 ){
+          vm.r[0xF] = vm.code[ ++vm.r[0xF] ];
+        } else{
+          vm.r[0xF]++;
+        }
+      } break;
+      
+      case IBCC:{
+        if( vm.flags & 0x20000000 ){
+          vm.r[0xF]++;
+        } else{
+          vm.r[0xF] = vm.code[ ++vm.r[0xF] ];
+        }
+      } break;
+      
+      case IBVS:{
+        if( vm.flags & 0x10000000 ){
+          vm.r[0xF] = vm.code[ ++vm.r[0xF] ];
+        } else{
+          vm.r[0xF]++;
+        }
+      } break;
+      
+      case IBVC:{
+        if( vm.flags & 0x10000000 ){
+          vm.r[0xF]++;
+        } else{
+          vm.r[0xF] = vm.code[ ++vm.r[0xF] ];
+        }
+      } break;
       
       case ISVC:{
         int32 proc = vm.code[ vm.r[0xF]++ ];
@@ -309,4 +460,103 @@ int main()
   }
   
   return(0);
+}
+
+inline void updflags_nz()
+{
+  _asm{
+    mov ebx, vm.flags
+    
+    lahf //stores the low byte of the flags into ah
+    
+    jns nclear
+    or ebx, 0x80000000
+    jmp nend
+    nclear: and ebx, 0x7fffffff
+    nend:
+    
+    sahf //restores flags from ah register
+    
+    jnz zclear
+    or ebx, 0x40000000
+    jmp zend
+    zclear: and ebx, 0xbfffffff
+    zend:
+    
+    mov vm.flags, ebx
+  }
+}
+
+inline void updflags_nzc()
+{
+  _asm{
+    mov ebx, vm.flags
+    
+    lahf
+    
+    jns nclear
+    or ebx, 0x80000000
+    jmp nend
+    nclear: and ebx, 0x7fffffff
+    nend:
+    
+    sahf
+    
+    jnz zclear
+    or ebx, 0x40000000
+    jmp zend
+    zclear: and ebx, 0xbfffffff
+    zend:
+    
+    sahf
+    
+    jnc cclear
+    or ebx, 0x20000000
+    jmp cend
+    cclear: and ebx, 0xdfffffff
+    cend:
+    
+    mov vm.flags, ebx
+  }
+}
+
+inline void updflags_nzcv()
+{
+  _asm{
+    mov ebx, vm.flags
+    
+    lahf
+    
+    jno vclear
+    or ebx, 0x10000000
+    jmp vend
+    vclear: and ebx, 0xefffffff
+    vend:
+    
+    sahf
+    
+    jns nclear
+    or ebx, 0x80000000
+    jmp nend
+    nclear: and ebx, 0x7fffffff
+    nend:
+    
+    sahf
+    
+    jnz zclear
+    or ebx, 0x40000000
+    jmp zend
+    zclear: and ebx, 0xbfffffff
+    zend:
+    
+    sahf
+    
+    jnc cclear
+    or ebx, 0x20000000
+    jmp cend
+    cclear: and ebx, 0xdfffffff
+    cend:
+    
+    mov vm.flags, ebx
+  }
 }
