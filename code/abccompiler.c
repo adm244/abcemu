184866d039a074d399db2ea5704cb10715677571
Dmitry Egorov <adm2444@gmail.com>
2017-03-15 16:53:27 +0300

    Source code release

--- code/abccompiler.c
+++ code/abccompiler.c
@@ -1,4 +1,29 @@
 /*
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/*
 INSTRUCTIONS:
   (IMPLEMENTED) LDR - loads constant into register
   (IMPLEMENTED) LDRM - loads value from memory into register
@@ -49,7 +74,7 @@ INSTRUCTIONS:
 
 #include "vm.h"
 
-#define LINESMAX 5
+#define LINESMAX 20
 #define CHARSMAX 255
 #define BUFFERSIZE LINESMAX*CHARSMAX
 
@@ -69,11 +94,14 @@ int main()
   
   char *buffer;
   char *strs[LINESMAX];
+  //char *labels[LINESMAX];
+  //uint labelnum = 0;
   
-  int i;
+  uint i;
   
   printf("Opening files...\n");
   
+  //TODO(adm244): write a console arguments parser
   input  = fopen("program.txt", "r");
   output = fopen("program.abc", "wb");
   
@@ -113,6 +141,8 @@ int main()
           printf("%s:\n", start);
           
           //TODO(adm244): labels!!
+          /*labels[labelnum++] = start;
+          labels[labelnum] = */
           
           continue;
         }
---
+++ readme.md
@@ -0,0 +1,6 @@
+##ABCEmu##
+
+### Description: ###
+
+A very primitive emulator of an abstract ARM-based processor. It was created as a student work for university.
+It's not entirely completed, but hope it can be usefull in some way or another to somebody.
